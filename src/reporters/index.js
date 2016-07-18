77c54531d4ee5a3dbb79d6b7486b8ab97528dc65
ezekielchentnik <zeek@ezekielchentnik.com>
2016-07-17 19:04:07 -0500

    update

--- src/reporters/index.js
+++
@@ -1,5 +0,0 @@
-import adobedtm from './adobedtm';
-import optimizely from './optimizely';
-import newrelic, { newrelicCrashReporter } from './newrelic';
-
-export { adobedtm, optimizely, newrelic, newrelicCrashReporter };
